c8a2679c2d1a52ed93e9e4448d43c9c906d48d19
salvicio@horsepower <emserfo@gmail.com>
2020-05-03 17:31:58 +0200

    octave try-catch errors openening fits

--- octave/one_spectra.m
+++ octave/one_spectra.m
@@ -1,20 +1,45 @@
 function [histpxl header dispersion hmean]=one_spectra
 
 pkg load fits
-spectra_folder='/home/salvicio/Documents/astro-shore/AO-CCD-FLAT-SPECTRUM/18102018_astro_obs-20181020T150328Z-001/spectra/';
-cd '/home/salvicio/Documents/astro-shore/AO-CCD-FLAT-SPECTRUM/18102018_astro_obs-20181020T150328Z-001/spectra/';
-files = dir( fullfile(spectra_folder,'*.fit') );
+spectra_folder=uigetdir ("/home/salvicio/", "Directory degli spettri")
+#spectra_folder='/home/salvicio/Documents/astro-shore/AO-CCD-FLAT-SPECTRUM/18102018_astro_obs-20181020T150328Z-001/spectra/';
+#cd '/home/salvicio/Documents/astro-shore/AO-CCD-FLAT-SPECTRUM/18102018_astro_obs-20181020T150328Z-001/spectra/';
+cd(spectra_folder)
+fullfile(spectra_folder,'/*.fits')
+files = dir(fullfile(spectra_folder,'/*.fits'));
 for i=1:numel({files.name}(:))
-[histpxl.(regexprep({files.name}(i),'.fit',''){:}) header.(regexprep({files.name}(i),'.fit',''){:})]=read_fits_image({files.name}(i){:});
-header.(regexprep({files.name}(i),'.fit',''){:})
-dispersion.(regexprep({files.name}(i),'.fit',''){:})=transpose(mean(histpxl.(regexprep({files.name}(i),'.fit',''){:}),2));
-hmean.(regexprep({files.name}(i),'.fit',''){:})=mean(histpxl.(regexprep({files.name}(i),'.fit',''){:}),1);
+try
+varname=genvarname(regexprep({files.name}(i),'.fits',''){:})
+[histpxl.(varname) header.(varname)]=read_fits_image({files.name}(i){:});
+header.(varname)
+dispersion.(varname)=transpose(mean(histpxl.(varname),2));
+hmean.(varname)=mean(histpxl.(varname),1);
+end_try_catch
 endfor
 
-struct_levels_to_print(0)
-histpxl
+
+#size(histpxl);
 cd '~/octave'
 
+#files.name
+#read_fits_image({files.name}(1){:});
+#fileswerr=[""];
+#errorcodes=[];
+#try #catchin errors opening files with fitsio
+#catch err
+#msg = lasterror.message;
+#if (strfind (msg, "operator *"))
+#number_of_errors++;
+#endif
+#fileswerr=[fileswerr {files.name}(i)];
+#errorcodes=[errorcodes err.identifier];
+#err.identifier
+#err.message
+#class(err.identifier)
+#err.identifier
+#end_try_catch
+
+#struct_levels_to_print(0)
 #function S(a), system(a); endfunction#
 #for filefits=fitsfilename
 #i
